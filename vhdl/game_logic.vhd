3b3ef4b8eb1015899968f13b76fd5fc991d8bca5
Aaron Storey <freaktm@vodafone.co.nz>
2010-12-02 14:28:22 +1300

    Started adding logic to control snake, 10% done

--- vhdl/game_logic.vhd
+++ vhdl/game_logic.vhd
@@ -30,27 +30,33 @@ use IEEE.numeric_std.ALL;
 
 entity game_logic is
   port( 
-		 clk50 : in std_logic;
+		 clk25 : in std_logic;
        ext_reset   : in  std_logic;
        WEA_int     : out std_logic;
        EN_int      : out std_logic;
-       address_a_int : out unsigned(12 downto 0);
-	    input_a_int   : out std_logic_vector(5 downto 0);
-       output_a_int   : in std_logic_vector(5 downto 0)
+       address_a_int : out unsigned(12 downto 0):= "0000000000000";
+	    input_a_int   : out unsigned(15 downto 0):= "0000000000000000";
+       output_a_int   : in unsigned(15 downto 0) := "0000000000000000";
+		 colour : out unsigned(1 downto 0);
+		 Direction : in std_logic_vector(1 downto 0)
 		 );
 end game_logic;
 
 architecture Behavioral of game_logic is
 
 
-signal head_cell : unsigned(12 downto 0) := "0100110001000"; -- cell 2440
-signal tail_cell : unsigned(12 downto 0) := "0100110001000"; -- cell 2440 (same start location as head)
-signal next_cell : unsigned(12 downto 0) := "0100100111000";  -- cell 2360
-signal birth_time : unsigned(4 downto 0) := "00101"; -- 5 seconds
+signal head_cell : unsigned(12 downto 0);-- := to_unsigned(2440, head_cell'length); -- cell 2440
+signal tail_cell : unsigned(12 downto 0);-- := to_unsigned(2440, tail_cell'length); -- cell 2440 (same start location as head)
+signal next_head_cell : unsigned(12 downto 0);-- := to_unsigned(2360, next_head_cell'length);  -- cell 2360
+signal next_tail_cell : unsigned(12 downto 0);-- := to_unsigned(2360, next_tail_cell'length);  -- cell 2360
+signal birth_time : unsigned(4 downto 0);-- := to_unsigned(5, birth_time'length); -- 5 seconds
 signal seconds : unsigned(19 downto 0);
 signal head_direction : unsigned(1 downto 0) := "00"; -- moving up
 signal tail_direction : unsigned(1 downto 0) := "00"; -- moving up
 signal speed : unsigned(4 downto 0) := "11111"; -- slowest speed
+signal score : unsigned(13 downto 0);
+signal timer : unsigned(13 downto 0);
+signal color : unsigned (1 downto 0);
                                       
 begin
 
@@ -58,9 +64,10 @@ begin
 
 
 EN_int <= '1';
+colour <= color;
  
 
- p_cell : process (clk50, ext_reset)
+ p_cell : process (clk25, ext_reset)
   begin  -- process p_cell
     if ext_reset = '1' then               -- asynchronous reset (active low)
       head_cell <= to_unsigned(2440, head_cell'length);
@@ -70,18 +77,46 @@ EN_int <= '1';
 		head_direction <= to_unsigned(0, head_direction'length); -- moving up
 		tail_direction <= to_unsigned(0, tail_direction'length); -- moving up
 		speed <= "11111";  -- slowest speed
-    elsif clk50'event and clk50 = '1' then    -- rising clock edge
+    elsif clk25'event and clk25 = '1' then    -- rising clock edge
                                       
-
------------------------GET CURRENT CELL  --------------------------------
-
------------------------GET CELL STATE   -------------------------------------------
-
+		
 
     end if;
   end process p_cell;
 
-
+-- p_timer : process (clk25, ext_reset)
+-- variable cnt: integer;
+-- begin
+--        if ext_reset = '1' then               -- asynchronous reset (active low)
+--        --add resets for timer and score
+--    elsif clk25'event and clk25 = '1' then    -- rising clock edge
+--cnt := cnt + 1;
+--if cnt = 25000000 then
+--color <= color + 1;
+--timer <= timer + 1;
+--cnt := 0;
+--end if;
+--end if;
+-- end process p_timer;
+ 
+ p_movesnake : process (clk25, ext_reset, Direction)
+ begin
+         if ext_reset = '1' then               -- asynchronous reset (active low)
+        --add resets for Direction
+    elsif clk25'event and clk25 = '1' then    -- rising clock edge
+		
+			if (Direction = "00") then
+			 color <= "00";
+			elsif (Direction = "01") then
+			 color <= "01";
+			elsif (Direction = "10") then
+			 color <= "10";
+			elsif (Direction = "11") then
+			 color <= "11";
+		end if;
+		end if;
+ 
+ end process p_movesnake;
 
 
 end Behavioral;
