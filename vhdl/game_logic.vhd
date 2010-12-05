f1ca4bfce1d2bedc9df7f2c7d106b889afee5a76
Aaron Storey <freaktm@vodafone.co.nz>
2010-12-05 21:57:31 +1300

    added more screen updating pipelines for score etc

--- vhdl/game_logic.vhd
+++ vhdl/game_logic.vhd
@@ -48,6 +48,10 @@ architecture Behavioral of game_logic is
   signal head_cell                                  : unsigned(12 downto 0)        := to_unsigned(2440, 13);  -- cell 2440
   signal tail_cell                                  : unsigned(12 downto 0)        := to_unsigned(2520, 13);  -- cell 2520 (cell below head cell)
   signal corner_cell                                : unsigned(12 downto 0);
+  signal score1_cell                                : unsigned(12 downto 0);
+  signal score2_cell                                : unsigned(12 downto 0);
+  signal score3_cell                                : unsigned(12 downto 0);
+  signal score4_cell                                : unsigned(12 downto 0);
   signal next_head_cell                             : unsigned(12 downto 0)        := to_unsigned(2360, 13);  -- cell 2360
   signal next_tail_cell                             : unsigned(12 downto 0)        := to_unsigned(2440, 13);  -- cell 2360
   signal speed                                      : unsigned(4 downto 0)         := "11111";  -- slowest speed
@@ -58,13 +62,17 @@ architecture Behavioral of game_logic is
   signal WE_head                                    : std_logic;
   signal WE_tail                                    : std_logic;
   signal WE_corner                                  : std_logic;
-  signal WE_score1, WE_score2, WE_score3, WE_score4 : std_logic                    := 0;  -- registers for the cell updater
+  signal WE_score1, WE_score2, WE_score3, WE_score4 : std_logic                    := '0';  -- registers for the cell updater
   signal next_direction                             : unsigned(2 downto 0);
   signal body_character                             : unsigned(12 downto 0)        := to_unsigned(3*8, 13);
   signal old_body_character                         : unsigned(12 downto 0);
   signal write_data_head                            : unsigned(15 downto 0);
   signal write_data_tail                            : unsigned(15 downto 0);
   signal write_data_corner                          : unsigned(15 downto 0);
+  signal write_data_score1                          : unsigned(15 downto 0);
+  signal write_data_score2                          : unsigned(15 downto 0);
+  signal write_data_score3                          : unsigned(15 downto 0);
+  signal write_data_score4                          : unsigned(15 downto 0);
   signal write_enable                               : std_logic;
   signal write_job                                  : std_logic_vector(2 downto 0) := "000";  -- signal for cell update pipeline
   
@@ -86,28 +94,43 @@ begin
   begin
     if ext_reset = '1' then             -- asynchronous reset (active low)
       current_direction <= "001";
-      head_cell         <= to_unsigned(2440, head_cell'length);
-      tail_cell         <= to_unsigned(2440, tail_cell'length);
       speed             <= "11111";     -- slowest speed
       skill             <= (others => '0');       -- lowest skill
-      body_character    <= to_unsigned(3*8, 13);  -- vertical
+      body_character    <= to_unsigned(3*8, 13);  -- 
       next_direction    <= "001";
       WE_head           <= '0';
       WE_corner         <= '0';
       WE_tail           <= '0';
-     
+      WE_score1         <= '0';
+      WE_score2         <= '0';
+      WE_score3         <= '0';
+      WE_score4         <= '0';
+      write_data_corner <= (others => '0');
+      write_data_head   <= (others => '0');
+      write_data_tail   <= (others => '0');
+      write_data_score1 <= (others => '0');
+      write_data_score2 <= (others => '0');
+      write_data_score3 <= (others => '0');
+      write_data_score4 <= (others => '0');
     elsif clk25'event and clk25 = '1' then        -- rising clock edge
 
 
---update display buffer for ram writer
+--update display buffer for ram writer process
       if (write_enable = '1') then
-        write_enable <= '0';
         if (write_job = "001") then
           WE_head <= '0';
         elsif (write_job = "010") then
           WE_corner <= '0';
         elsif (write_job = "011") then
           WE_tail <= '0';
+        elsif (write_job = "100") then
+          WE_score1 <= '0';
+        elsif (write_job = "101") then
+          WE_score2 <= '0';
+        elsif (write_job = "110") then
+          WE_score3 <= '0';
+        elsif (write_job = "111") then
+          WE_score4 <= '0';
         end if;
       end if;
 
@@ -209,8 +232,6 @@ begin
         end if;
         
       end if;
-
-      
     end if;
     
   end process p_movesnake;
@@ -219,12 +240,15 @@ begin
   -- purpose: updates the ram entries for the video display
   -- type   : sequential
   -- inputs : clk25, ext_reset, WE_head, WE_tail, WE_corner, write_data_head,
-  -- write_data_tail, write_data_corner, head_cell, corner_cell, tail_cell
+  -- write_data_tail, write_data_corner, head_cell, corner_cell, tail_cell,
+  -- WE_score1, WE_Score2, WE_score3, WE_score4, write_data_score1, write_data_score2, write_data_score3, write_data_score4
   -- outputs : address_a_int, write_enable, input_a_int, write_job
   p_cellupdate : process (clk25, ext_reset)
   begin  -- process p_cellupdate
     if ext_reset = '1' then             -- asynchronous reset (active lo high)
-      write_job <= (others => '0');
+     write_job <= (others => '0');
+	  head_cell         <= to_unsigned(2440, head_cell'length);
+     tail_cell         <= to_unsigned(2440, tail_cell'length);
     elsif clk25'event and clk25 = '1' then  -- rising clock edge 
       if (WE_head = '1') then
         write_job     <= "001";
@@ -240,29 +264,32 @@ begin
         write_enable  <= '1';
       elsif (WE_tail = '1') then
         write_job     <= "011";
-        input_a_int   <= write_data_corner;
-        address_a_int <= corner_cell;
+        input_a_int   <= write_data_tail;
+        address_a_int <= tail_cell;
         write_enable  <= '1';
       elsif (WE_score1 = '1') then
         write_job     <= "100";
-        input_a_int   <= write_data_corner;
-        address_a_int <= corner_cell;
+        input_a_int   <= write_data_score1;
+        address_a_int <= score1_cell;
         write_enable  <= '1';
       elsif (WE_score2 = '1') then
         write_job     <= "101";
-        input_a_int   <= write_data_corner;
-        address_a_int <= corner_cell;
+        input_a_int   <= write_data_score2;
+        address_a_int <= score2_cell;
         write_enable  <= '1';
       elsif (WE_score3 = '1') then
         write_job     <= "110";
-        input_a_int   <= write_data_corner;
-        address_a_int <= corner_cell;
+        input_a_int   <= write_data_score3;
+        address_a_int <= score3_cell;
         write_enable  <= '1';
       elsif (WE_score4 = '1') then
         write_job     <= "111";
-        input_a_int   <= write_data_corner;
-        address_a_int <= corner_cell;
+        input_a_int   <= write_data_score4;
+        address_a_int <= score4_cell;
         write_enable  <= '1';
+       else
+		   write_enable <= '0';
+         write_job <= (others => '0');
       end if;
     end if;
   end process p_cellupdate;
