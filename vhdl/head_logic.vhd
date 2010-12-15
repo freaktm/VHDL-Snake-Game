c6837ed4d6105237e6fc7624dd6f8aa2243a52f4
CompSci temp account 2 <cs002@nui.cs.waikato.ac.nz>
2010-12-15 15:44:45 +1300

    minor changes

--- vhdl/head_logic.vhd
+++ vhdl/head_logic.vhd
@@ -32,12 +32,12 @@ use work.gamelogic_pkg.all;
 
 entity head_logic is
   port(
-  gamelogic_state   : in  gamelogic_state_t;
-    address_a_head : out unsigned(12 downto 0);
-    head_write_data   : out unsigned(11 downto 0);
-    head_done     : out std_logic;
-	 next_cell : in unsigned(12 downto 0);
-	 current_direction_in : in unsigned(2 downto 0)
+    gamelogic_state      : in  gamelogic_state_t;
+    address_a_head       : out unsigned(12 downto 0);
+    head_write_data      : out unsigned(11 downto 0);
+    head_done            : out std_logic;
+    next_cell            : in  unsigned(12 downto 0);
+    current_direction_in : in  unsigned(2 downto 0)
     );
 end head_logic;
 
@@ -45,27 +45,27 @@ architecture Behavioral of head_logic is
 
 
   signal snake_character : unsigned(8 downto 0);
- 
+  
   
 begin
   
- 
   
-p_update_character : process (gamelogic_state, current_direction_in, snake_character)
-begin
-   if (gamelogic_state = HEAD) then
-	if (current_direction_in = "001") or (current_direction_in = "011") then
-	 snake_character <= to_unsigned(3*8, snake_character'length);
-	 elsif (current_direction_in = "010") or (current_direction_in = "100") then
-	 snake_character <= to_unsigned(2*8, snake_character'length);
-	 end if;
-	address_a_head <= next_cell;
-	head_write_data <= current_direction_in & snake_character;
-	head_done <= '1';
- else
- head_done <= '0';	
- end if;
-end process p_update_character;
+  
+  p_update_character : process (gamelogic_state, current_direction_in, snake_character)
+  begin
+    if (gamelogic_state = HEAD) then
+      if (current_direction_in = "001") or (current_direction_in = "011") then
+        snake_character <= to_unsigned(3*8, snake_character'length);
+      elsif (current_direction_in = "010") or (current_direction_in = "100") then
+        snake_character <= to_unsigned(2*8, snake_character'length);
+      end if;
+      address_a_head  <= next_cell;
+      head_write_data <= current_direction_in & snake_character;
+      head_done       <= '1';
+    else
+      head_done <= '0';
+    end if;
+  end process p_update_character;
 
 
-    end Behavioral;
+end Behavioral;
