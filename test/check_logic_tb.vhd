cc8d0afbeb704218ebe159658832983e728a0f72
Aaron Storey <freaktm@vodafone.co.nz>
2010-12-24 15:09:24 +1300

    moved testbench files to test folder

--- test/check_logic_tb.vhd
+++ test/check_logic_tb.vhd
@@ -6,7 +6,7 @@
 -- Author     : CompSci temp account 2  <cs002@nui.cs.waikato.ac.nz>
 -- Company    : 
 -- Created    : 2010-12-17
--- Last update: 2010-12-17
+-- Last update: 2010-12-24
 -- Platform   : 
 -- Standard   : VHDL'87
 -------------------------------------------------------------------------------
@@ -96,7 +96,7 @@ begin  -- tb
        ext_reset <= '1';
        wait for 100 ns;
        ext_reset <= '0';
-       wait for 500 ns;
+       wait for 1000 ns;
        keyboard <= "010";
        
        wait;
