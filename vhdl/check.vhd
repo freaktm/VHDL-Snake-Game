d9da22da49375b1aa2613d676ece03f10e861000
Aaron Storey <freaktm@vodafone.co.nz>
2010-12-24 15:01:34 +1300

    added extra pipeline for memory read

--- vhdl/check.vhd
+++ vhdl/check.vhd
@@ -55,57 +55,63 @@ begin
 
   next_direction <= keyboard;
   next_cell      <= next_cell_int;
+  --       current_cell    <= next_cell_int;
 
 
   --purpose: checks if the snake has crashed into a border or itself
   --type   : sequential
   --inputs : clk25, ext_reset, state, next_direction, output_a_int, crash_result_ready
   --outputs: crash_test, crashed
-  p_collision_checker : process (clk25, ext_reset)
+  p_collision_checker : process (clk25, ext_reset, gamelogic_state)
   begin  -- process p_collision_checker
     if (ext_reset = '1') then           --  asynchronous reset (active high)
       crashed               <= '0';
       check_done            <= '0';
-      checking              <= "000";
+      checking              <= "00";
       current_cell          <= to_unsigned(2520, current_cell'length);
       current_direction_int <= "001";   -- reset to moving up
       next_cell_int         <= to_unsigned(2440, next_cell_int'length);
-    elsif (clk25'event and clk25 = '1') and (gamelogic_state = CHECK) then  --     rising clock edge
-      old_direction_out <= current_direction_int;
-      if (current_direction_int /= next_direction) then
-        nochange <= '0';
-      else
-        nochange <= '1';
-      end if;
-      if (checking = "000") then
-        checking              <= "001";
-        current_direction_int <= next_direction;
-        if (next_direction = "001") then
-          next_cell_int <= to_unsigned(to_integer(current_cell) - 80, next_cell_int'length);
-        elsif (next_direction = "010") then
-          next_cell_int <= to_unsigned(to_integer(current_cell) + 1, next_cell_int'length);
-        elsif (next_direction = "011") then
-          next_cell_int <= to_unsigned(to_integer(current_cell) + 80, next_cell_int'length);
-        elsif (next_direction = "100") then
-          next_cell_int <= to_unsigned(to_integer(current_cell) - 1, next_cell_int'length);
-        end if;
-        current_cell    <= next_cell_int;
-        address_a_check <= next_cell_int;
-      elsif (checking = "001") then
-        checking <= "010";
-        if (to_integer(check_read_data) /= 0) then
-          crashed <= '1';
+    elsif (clk25'event and clk25 = '1') then
+      if (gamelogic_state = CHECK) then
+        old_direction_out <= current_direction_int;
+        if (current_direction_int /= next_direction) then
+          nochange <= '0';
         else
-          crashed <= '0';
+          nochange <= '1';
         end if;
-      elsif (checking = "010") then
-        checking   <= "000";
-        check_done <= '1';
+        if (checking = "00") then
+          check_done            <= '0';
+          checking              <= "01";
+          current_direction_int <= next_direction;
+          if (next_direction = "001") then
+            next_cell_int <= to_unsigned(to_integer(current_cell) - 80, next_cell_int'length);
+          elsif (next_direction = "010") then
+            next_cell_int <= to_unsigned(to_integer(current_cell) + 1, next_cell_int'length);
+          elsif (next_direction = "011") then
+            next_cell_int <= to_unsigned(to_integer(current_cell) + 80, next_cell_int'length);
+          elsif (next_direction = "100") then
+            next_cell_int <= to_unsigned(to_integer(current_cell) - 1, next_cell_int'length);
+          end if;
+        elsif (checking = "01") then
+          checking        <= "10";
+          address_a_check <= next_cell_int;
+           current_cell    <= next_cell_int;
+        elsif (checking = "10") then
+          checking <= "11";
+        elsif (checking = "11") then
+          if (to_integer(check_read_data) /= 0) then
+            crashed <= '1';
+          else
+            crashed <= '0';
+          end if;
+          checking   <= "00";
+          check_done <= '1';
+        end if;
+      else
+        check_done <= '0';
+        crashed    <= '0';
+        checking   <= "00";
       end if;
-    else
-      check_done <= '0';
-      crashed    <= '0';
-      checking   <= "000";
     end if;
   end process p_collision_checker;
 
