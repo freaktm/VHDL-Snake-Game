565d4101f795cf4e451896e81826676df8fc0ada
Aaron Storey <freaktm@vodafone.co.nz>
2010-12-31 00:01:11 +1300

    inits and internal signals

--- vhdl/corner.vhd
+++ vhdl/corner.vhd
@@ -34,14 +34,16 @@ end corner_logic;
 architecture Behavioral of corner_logic is
 
 
-  signal snake_character : unsigned(8 downto 0);
-  signal counting        : std_logic_vector(1 downto 0) := "00";
-  
+  signal snake_character       : unsigned(8 downto 0)         := (others => '0');
+  signal counting              : std_logic_vector(1 downto 0) := "00";
+  signal corner_done_int       : std_logic                    := '0';
+  signal corner_write_data_int : unsigned(11 downto 0)        := (others <= '0');
   
 begin
   
-  address_a_corner <= next_cell;
-
+  address_a_corner  <= next_cell;
+  corner_done       <= corner_done_int;
+  corner_write_data <= corner_write_data_int;
 
   -- purpose: generates the corner cell
   -- type   : sequential
@@ -50,8 +52,10 @@ begin
   p_process_corner : process (clk_slow, ext_reset)
   begin  -- process p_process_corner
     if ext_reset = '1' then             -- asynchronous reset (active high)
-      snake_character <= to_unsigned(5*8, snake_character'length);
-      corner_done     <= '0';
+      snake_character       <= to_unsigned(5*8, snake_character'length);
+      corner_done_int       <= '0';
+      counting              <= "00";
+      corner_write_data_int <= (others => '0');
     elsif clk_slow'event and clk_slow = '1' then  -- rising clock edge
       if (gamelogic_state = CORNER) then
         if ((current_direction_in = "001") and (old_direction_in = "010")) or ((current_direction_in = "100") and (old_direction_in = "011")) then
@@ -66,10 +70,10 @@ begin
           snake_character <= to_unsigned(7*8, snake_character'length);
         end if;
 
-        corner_write_data <= current_direction_in & snake_character;
-        corner_done       <= '1';
+        corner_write_data_int <= current_direction_in & snake_character;
+        corner_done_int       <= '1';
       else
-        corner_done <= '0';
+        corner_done_int <= '0';
       end if;
     end if;
   end process p_process_corner;
