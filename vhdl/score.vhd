d8c1d29b9aa518faa9cc0102f0ea8973347aae07
Aaron Storey <freaktm@vodafone.co.nz>
2010-12-31 01:18:58 +1300

    internals and inits

--- vhdl/score.vhd
+++ vhdl/score.vhd
@@ -28,12 +28,14 @@ end score_logic;
 architecture Behavioral of score_logic is
 
 
-  signal score_int : unsigned(13 downto 0);
+  signal score_int      : unsigned(13 downto 0);
+  signal score_done_int : std_logic := '1';  --scoring disabled
 
 begin
-  
---  score      <= score_int;
---  score_done <= '1';
+
+  score      <= score_int;
+  score_done <= score_done_int;
+
 
 
 end Behavioral;
