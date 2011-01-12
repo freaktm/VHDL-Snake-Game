56ab4885beec51f12da7e0e0179cc713af7be7fb
Aaron Storey <freaktm@vodafone.co.nz>
2011-01-13 03:00:25 +1300

    added state machine

--- vhdl/check.vhd
+++ vhdl/check.vhd
@@ -42,15 +42,17 @@ architecture Behavioral of check_logic is
   signal current_cell          : unsigned(12 downto 0) := to_unsigned(2440, 13);
   signal next_cell_int         : unsigned(12 downto 0) := to_unsigned(2360, 13);
   signal corner_cell_int       : unsigned(12 downto 0) := (others => '0');
-  signal checking              : std_logic             := '0';
   signal old_direction_out_int : unsigned(2 downto 0)  := "001";
   signal nochange_int          : std_logic             := '1';
   signal crashed_int           : std_logic             := '0';
   signal check_done_int        : std_logic             := '0';
 
+  type   check_state_t is (IDLE, CHECK_DIRECTION, CHECK_HIT);
+  signal check_state : check_state_t := IDLE;
   
 begin
-  
+
+
   old_direction_out     <= old_direction_out_int;
   current_direction_out <= current_direction_int;
   address_a_check       <= next_cell_int;
@@ -64,6 +66,9 @@ begin
   next_cell      <= next_cell_int;
 
 
+
+
+
   --purpose: checks if the snake has crashed into a border or itself
   --type   : sequential
   --inputs : clk25, ext_reset, state, next_direction, output_a_int, crash_result_ready
@@ -71,39 +76,43 @@ begin
   p_collision_checker : process (clk_slow, ext_reset)
   begin  -- process p_collision_checker
     if (ext_reset = '1') then           --  asynchronous reset (active high)
-      crashed_int           <= '0';
-      check_done_int        <= '0';
-      nochange_int          <= '1';
-      checking              <= '0';
+      
+      crashed_int    <= '0';
+      check_done_int <= '0';
+      nochange_int   <= '1';
+
       current_cell          <= to_unsigned(2440, current_cell'length);
       current_direction_int <= "001";   -- reset to moving up
       next_cell_int         <= to_unsigned(2360, next_cell_int'length);
+
       old_direction_out_int <= "001";
       corner_cell_int       <= (others => '0');
+      check_state           <= IDLE;
+      
     elsif (clk_slow'event and clk_slow = '1') then
       if (gamelogic_state = CHECK) then
-        if (checking = '0') then
-          checking <= '1';
+        if (check_state = IDLE) then
+          check_state <= CHECK_DIRECTION;
           -- checks to see if the snake has changed direction
           if (current_direction_int /= next_direction) then
             old_direction_out_int <= current_direction_int;
             current_direction_int <= next_direction;
             corner_cell_int       <= current_cell;
             nochange_int          <= '0';
-          else
-            if (next_direction = "001") then
-              next_cell_int <= to_unsigned(to_integer(current_cell) - 80, next_cell_int'length);
-            elsif (next_direction = "010") then
-              next_cell_int <= to_unsigned(to_integer(current_cell) + 1, next_cell_int'length);
-            elsif (next_direction = "011") then
-              next_cell_int <= to_unsigned(to_integer(current_cell) + 80, next_cell_int'length);
-            elsif (next_direction = "100") then
-              next_cell_int <= to_unsigned(to_integer(current_cell) - 1, next_cell_int'length);
-            end if;
           end if;
-        else
-          checking     <= '0';
-          current_cell <= next_cell_int;
+        elsif (check_state = CHECK_DIRECTION) then
+          check_state <= CHECK_HIT;
+          if (next_direction = "001") then
+            next_cell_int <= to_unsigned(to_integer(current_cell) - 80, next_cell_int'length);
+          elsif (next_direction = "010") then
+            next_cell_int <= to_unsigned(to_integer(current_cell) + 1, next_cell_int'length);
+          elsif (next_direction = "011") then
+            next_cell_int <= to_unsigned(to_integer(current_cell) + 80, next_cell_int'length);
+          elsif (next_direction = "100") then
+            next_cell_int <= to_unsigned(to_integer(current_cell) - 1, next_cell_int'length);
+          end if;
+        elsif (check_state = CHECK_HIT) then
+          check_state <= IDLE;
           if (to_integer(check_read_data) = 0) then
             check_done_int <= '1';
           else
@@ -111,10 +120,11 @@ begin
           end if;
         end if;
       else
+        check_state    <= IDLE;
+        current_cell   <= next_cell_int;
         crashed_int    <= '0';
         check_done_int <= '0';
         nochange_int   <= '1';
-        checking       <= '0';
       end if;
     end if;
   end process p_collision_checker;
