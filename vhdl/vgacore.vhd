96318f7bf1555411ae5dc053d851a216c7a1f818
Aaron Storey <freaktm@vodafone.co.nz>
2011-01-13 02:59:33 +1300

    redone vgacore again, using modelsim as guide

--- vhdl/vgacore.vhd
+++ vhdl/vgacore.vhd
@@ -54,14 +54,16 @@ architecture behavioral of vga_core is
   signal   row_data_signal      : unsigned(7 downto 0)  := (others => '0');
   signal   red, green           : std_logic             := '0';
   signal   blue                 : std_logic             := '0';
-  constant HB                   : integer               := 48;
-  constant HF                   : integer               := 16;
+  constant HB                   : integer               := 38;
+  constant HF                   : integer               := 26;
   constant HS                   : integer               := 96;
   constant HD                   : integer               := 640;
   constant VB                   : integer               := 33;
   constant VF                   : integer               := 10;
   constant VS                   : integer               := 2;
   constant VD                   : integer               := 480;
+  constant H_WIDTH              : integer               := HB+HF+HS+HD-1;  -- screen counter horizontal
+  constant V_WIDTH              : integer               := VB+VF+VS+VD-1;  -- screen counter vertical
 
 
 begin
@@ -87,16 +89,15 @@ begin
       row_count     <= (others => '0');
     elsif clk25'event and clk25 = '1' then  -- rising clock edge
       -- increment counters
-      if (to_integer(hcounter) < 799) then
+      if (to_integer(hcounter) < H_WIDTH) then
         hcounter <= hcounter + 1;
       else
         hcounter <= (others => '0');
-        if (to_integer(vcounter) < 523) then
-          if (to_integer(vcounter) > (VB + VS)) then
+        if (to_integer(vcounter) < V_WIDTH) then
+          if (to_integer(vcounter) > (VB + VS - 2))
+            and (to_integer(hcounter) = (H_WIDTH - 8))
+          then
             row_count <= row_count + 1;
-            if row_count = "111" then
-              row_count <= (others => '0');
-            end if;
           end if;
           vcounter <= vcounter + 1;
         else
@@ -104,58 +105,63 @@ begin
           row_count <= (others => '0');
         end if;
       end if;
+
+
+      -- displays pixel data, in blue colour
+      if (to_integer(hcounter) > (HB + HS - 1))
+        and (to_integer(hcounter) < (HB + HS + HD))
+        and (to_integer(vcounter) > (VB + VS - 1))
+        and (to_integer(vcounter) < (VB + VS + VD))
+      then
+        red   <= '0';
+        green <= '0';
+        blue  <= '1';
+      else
+        red   <= '0';
+        green <= '0';
+        blue  <= '0';
+      end if;
+
+
+      -- define synch pulse's
+      if (to_integer(hcounter) > HB - 1) and
+        (to_integer(hcounter) < (HB + HS - 1)) then
+        hs_out_signal <= '0';
+      else
+        hs_out_signal <= '1';
+      end if;
+
+
+      if (to_integer(vcounter) > VB - 1) and
+        (to_integer(vcounter) < (VB + VS - 1)) then
+        vs_out_signal <= '0';
+      else
+        vs_out_signal <= '1';
+      end if;
     end if;
-
-
-    -- displays pixel data, in blue colour
-    if (to_integer(hcounter) > (HB + HS))
-      and (to_integer(hcounter) < (HB + HS + HD))
-      and (to_integer(vcounter) > (VB + VS))
-      and (to_integer(vcounter) < (VB + VS + VD))
-    then
-      red   <= '0';
-      green <= '0';
-      blue  <= pixel;
-    else
-      red   <= '0';
-      green <= '0';
-      blue  <= '0';
-    end if;
-
-
-    -- define synch pulse's
-    if (to_integer(hcounter) > HB) and
-      (to_integer(hcounter) < (HB + HS)) then
-      hs_out_signal <= '0';
-    else
-      hs_out_signal <= '1';
-    end if;
-
-
-    if (to_integer(vcounter) > VB) and
-      (to_integer(vcounter) < (VB + VS)) then
-      vs_out_signal <= '0';
-    else
-      vs_out_signal <= '1';
-    end if;
-
-
-    x      <= to_unsigned(to_integer(hcounter) - (HB + HS), x'length);
-    y      <= to_unsigned(to_integer(vcounter) - (VB + VS), y'length);
-    x_temp <= x(9 downto 3);
-    y_temp <= y(8 downto 3);
-
-    if (to_integer(x) < 1) and (to_integer(y) < 1) then
-      cell <= (others => '0');
-    else
-      cell <= to_unsigned(((to_integer(x_temp)) + (to_integer(y_temp) * 80)), cell'length);
-    end if;
-    
   end process p_vga_signals;
 
 
 
-
+  p_cell_counter : process (hcounter, vcounter, pixelcount_w)
+  begin  -- process p_cell_counter
+    if ext_reset = '1' then
+      cell <= (others => '0');
+    else
+      if (to_integer(hcounter) > (HB+HS-2))
+        and (to_integer(hcounter) < (HB+HS+HD))
+        and (to_integer(vcounter) < (VB+VS+VD))
+        and (to_integer(vcounter) > (VS+VB-2)) then
+        if pixelcount_w = "000" then
+          if (to_integer(cell) < 4799) then
+            cell <= cell + 1;
+          else
+            cell <= (others => '0');
+          end if;
+        end if;
+      end if;
+    end if;
+  end process p_cell_counter;
 
 
 
@@ -167,20 +173,6 @@ begin
       rom_address_signal   <= (others => '0');
       row_data_signal      <= (others => '0');
     elsif clk25'event and clk25 = '1' then
-      
-      if (to_integer(hcounter) = 154) and (to_integer(vcounter) = 43) then
-        strobe_signal        <= '0';
-        ram_address_b_signal <= (others => '0');
-      elsif (to_integer(hcounter) = 155) and (to_integer(vcounter) = 43) then
-        strobe_signal      <= '0';
-        rom_address_signal <= to_unsigned(to_integer(ram_data_b(8 downto 0)) + to_integer(row_count), rom_address'length);
-      elsif (to_integer(hcounter) = 156) and (to_integer(vcounter) = 43) then
-        strobe_signal   <= '0';
-        row_data_signal <= rom_data;
-      elsif (to_integer(hcounter) = 162) and (to_integer(vcounter) = 43) then
-        strobe_signal <= '1';
-      end if;
-
       if pixelcount_w = "111" then
         strobe_signal <= '1';
       elsif pixelcount_w < "100" then
@@ -203,8 +195,8 @@ begin
       pixelcount_w <= (others => '0');
 
     elsif clk25'event and clk25 = '1' then
-      if (to_integer(hcounter) < (HB + HS))
-        or (to_integer(vcounter) < (VB + VS)) then
+      if (to_integer(hcounter) < (HB + HS - 24))
+        or (to_integer(vcounter) < (VB + VS - 2)) then
         pixelcount_w <= (others => '0');
       else
         pixelcount_w <= pixelcount_w + 1;
